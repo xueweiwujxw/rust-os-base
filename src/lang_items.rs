1ce99d933e6d6a0297b9bcdad78dc54b48901878
xueweiwujxw <xueweiwujxw@outlook.com>
2022-04-13 17:04:34 +0800

    ✨ feat(all): Hello world

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,7 +1,18 @@
+use crate::{println, sbi::shutdown};
 use core::panic::PanicInfo;
 
 #[panic_handler]
 
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    if let Some(location) = _info.location() {
+        println!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            _info.message().unwrap()
+        );
+    } else {
+        println!("Panicked: {}", _info.message().unwrap());
+    }
+    shutdown()
 }
